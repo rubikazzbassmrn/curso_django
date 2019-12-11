0628301989789752cf20976ef1f1f26cdbfb8618
rubikazzbassmrn <rubikazzbassmrn@gmail.com>
2019-12-11 16:10:09 -0600

    Avance 11122019

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name = "Titulo")
+    description = models.TextField(verbose_name = "Descripcion")
+    image = models.ImageField(verbose_name = "Imagen")
+    created = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de creacion")
+    updated = models.DateTimeField(auto_now=True, verbose_name = "Fecha de edicion")
+    class Meta:
+        verbose_name = 'Proyecto'
+        verbose_name_plural = 'Proyectos'
+        ordering = ["-created"]
+    def __str__(self):
+        return self.title
