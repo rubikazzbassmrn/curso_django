7347ee308b2a5650c20ab260b4dd1fd43058ad35
rubikazzbassmrn <rubikazzbassmrn@gmail.com>
2019-12-24 00:50:17 -0600

    Avance 24122019

---
+++ WEB_EMPRESARIAL/webempresarial/core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Inicio")
+def about(request):
+    return HttpResponse("Acerca")
+def services(request):
+    return HttpResponse("Servicios")
+def store(request):
+    return HttpResponse("Tienda")
+def contact(request):
+    return HttpResponse("Contacto")
+def blog(request):
+    return HttpResponse("Blog")
+def sample(request):
+    return HttpResponse("Sample")
